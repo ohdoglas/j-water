e586c2fd0ac744c8bab740902c4d8e8cc03da458
Douglas Morais <dughlaisdd@gmail.com>
2024-04-12 20:35:16 -0300

    Polimento calculadora

--- j-water/indexjs/index.js
+++ j-water/indexjs/index.js
@@ -1,1 +1,106 @@
 // J-water
+
+function waterCon () {
+    let peso = 95
+    let altura = 176
+    let idade = 29
+
+    let masculino = 1
+    let feminino = 0
+
+    let sexo
+
+    let nivelAtividade
+//!tmb homem
+
+    let tmbh = 88.362 + (13.397 * peso) + (4.799 * altura) - (5.677 * idade)
+    let tmbm = 447.593 + (9.247 * peso) + (3.098 * altura) - (4.330 * idade)
+
+    if (sexo){
+    console.log("Ja que você é homem, sua taxa metabólica basal é:", tmbh)
+}   else {
+    console.log("Ja que você é mulher, sua taxa metabólica basal é:", tmbm)
+}
+
+
+//!tmb mulher
+
+
+
+
+
+
+//!nivel de atividade fisica
+// let sedentario = tmbh * 1.375
+
+// if (nivelAtividade = 1) {
+//     console.log(sedentario)
+// }
+
+}
+
+
+
+
+
+let calculoJwater
+
+// !calculo MTB
+
+
+
+
+let peso = 95
+let altura = 176
+let idade = 29
+
+let masculino = 1
+let feminino = 0
+
+let sexo
+
+let nivelAtividade
+//!tmb homem
+
+let tmbh = 88.362 + (13.397 * peso) + (4.799 * altura) - (5.677 * idade)
+let tmbm = 447.593 + (9.247 * peso) + (3.098 * altura) - (4.330 * idade)
+
+if (sexo){
+    console.log("Ja que você é homem, sua taxa metabólica basal é:", tmbh)
+}   else {
+    console.log("Ja que você é mulher, sua taxa metabólica basal é:", tmbm)
+}
+
+
+//!tmb mulher
+
+
+
+
+
+
+//!nivel de atividade fisica
+// let sedentario = tmbh * 1.375
+
+// if (nivelAtividade = 1) {
+//     console.log(sedentario)
+// }
+
+
+
+
+
+
+
+let sexoSelection = document.querySelectorAll
+("input[name='sexo']")
+let resultSexo = document.getElementById("");
+radioBtns.forEach(radioBtn => {});
+// const sexomasculino = document.Jwater.elements["m"].value;
+// const sexofeminino = document.Jwater.elements["f"].value;
+
+if (sexoSelection = m){
+alert("Ja que você é homem, sua taxa metabólica basal é:", tmbh)
+}   else if (sexofeminino) {
+alert("Ja que você é mulher, sua taxa metabólica basal é:", tmbm)
+}
